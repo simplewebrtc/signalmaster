e025e30f69a638b2c26727240e39cdd3c58e816c
Henrik Joreteg <hjoreteg@gmail.com>
2013-02-05 00:55:49 -0800

    adding support for rooms

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 /*global console*/
 var yetify = require('yetify'),
     config = require('getconfig'),
+    uuid = require('node-uuid'),
     io = require('socket.io').listen(config.server.port);
 
 io.sockets.on('connection', function (client) {
+    // pass a message
     client.on('message', function (details) {
         var otherClient = io.sockets.sockets[details.to];
 
@@ -13,6 +15,43 @@ io.sockets.on('connection', function (client) {
         delete details.to;
         details.from = client.id;
         otherClient.emit('message', details);
+    });
+
+    client.on('join', function (name) {
+        client.join(name);
+        io.sockets.in(name).emit('joined', {
+            room: name,
+            id: client.id
+            //people: io.sockets.clients(name)
+        });
+    });
+
+    client.on('disconnect', function () {
+        var rooms = io.sockets.manager.roomClients[client.id];
+        for (var name in rooms) {
+            if (name) {
+                io.sockets.in(name.slice(1)).emit('left', {
+                    room: name,
+                    id: client.id
+                });
+            }
+        }
+    });
+
+    client.on('create', function (name, cb) {
+        if (arguments.length == 2) {
+            cb = typeof cb == 'function' ? cb : function () {};
+        } else {
+            cb = name;
+            name = uuid();
+        }
+        // check if exists
+        if (io.sockets.clients(name).length) {
+            cb('taken');
+        } else {
+            client.join(name);
+            cb(null, name);
+        }
     });
 });
 

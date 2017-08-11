870b421f5c2b93956ffe08d453ef9c9a72462155
Lance Stout <lancestout@gmail.com>
2017-08-11 09:29:44 -0700

    Fix linting

--- scripts/create-configmap.js
+++ scripts/create-configmap.js
@@ -1,20 +1,32 @@
-const fs = require('fs');
-var Exec = require('child_process').exec
+'use strict';
 
-fs.readFile('/etc/prosody/prosody.cfg.lua', 'utf8', function(err, data) {
-	if (err) {
-		return console.log(err);
-	}
+const FS = require('fs');
+const Exec = require('child_process').exec;
 
-	var prosodyConfig = data.toString().replace(/\"/g, '\'').replace(/\n/g, '\\n');
-	var configMapTemplate = `{ \n "apiVersion": "v1", \n "kind": "ConfigMap", \n "metadata": { \n "name": "prosody-config",\n"namespace": "default"\n},\n"data": {\n"prosody.cfg.lua": "${prosodyConfig}"\n	  }\n	}`;
-	fs.writeFile('./cm-prosody-config.json', configMapTemplate, (err) => {
-		if (err) throw err;
-		console.log('Config Written');
-		var postConfigMap = Exec('/bin/sh /app/scripts/post-prosody-config.sh', function(err, stdout, stderr) {
-			if (err) console.log(err);
-			process.stdout.write(stdout);
-			process.stderr.write(stderr);
-		});
-	});
+FS.readFile('/etc/prosody/prosody.cfg.lua', 'utf8', (err, data) => {
+
+  if (err) {
+    return console.log(err);
+  }
+
+  const prosodyConfig = data.toString().replace(/\"/g, '\'').replace(/\n/g, '\\n');
+  const configMapTemplate = `{ \n "apiVersion": "v1", \n "kind": "ConfigMap", \n "metadata": { \n "name": "prosody-config",\n"namespace": "default"\n},\n"data": {\n"prosody.cfg.lua": "${prosodyConfig}"\n     }\n   }`;
+
+  FS.writeFile('./cm-prosody-config.json', configMapTemplate, (err) => {
+
+    if (err) {
+      throw err;
+    }
+
+    console.log('Config Written');
+    Exec('/bin/sh /app/scripts/post-prosody-config.sh', (err, stdout, stderr) => {
+
+      if (err) {
+        console.log(err);
+      }
+
+      process.stdout.write(stdout);
+      process.stderr.write(stderr);
+    });
+  });
 });

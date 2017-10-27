870337c14af4749913b22a623f3680798a6812a4
Michael Garvin <gar+gh@danger.computer>
2017-10-27 11:40:35 -0700

    only delete what we have to during tests

--- test/event_worker.js
+++ test/event_worker.js
@@ -9,28 +9,38 @@ const { eventWorker, redis, db } = Fixtures;
 
 const lab = exports.lab = Lab.script();
 
-const { describe, it, after } = lab;
+const { describe, it, before, after } = lab;
 const { expect } = Code;
 
 const rpush = promisify(redis.rpush);
 
 describe('event worker', () => {
 
+  const events = [];
+  const main_event = Fixtures.event({ type: 'room_created' });
+  for (let i = 0; i < 10; i++) {
+    const event = Fixtures.event({ room_id: main_event.room_id, type: `action_${i}` });
+    events.push(event);
+  }
+
+  before(async () => {
+
+    await rpush('events', JSON.stringify(main_event));
+    for (const event of events) {
+      await rpush('events', JSON.stringify(event));
+    }
+  });
+
   after(async () => {
 
-    await db.events.destroy();
+    await db.events.destroy(main_event);
+    for (const event of events) {
+      await db.events.destroy(event);
+    }
   });
 
   it('processes events', async () => {
 
-    const main_event = Fixtures.event({ type: 'room_created' });
-    await rpush('events', JSON.stringify(main_event));
-    const events = [];
-    for (let i = 0; i < 10; i++) {
-      const event = Fixtures.event({ room_id: main_event.room_id, type: `action_${i}` });
-      events.push(event);
-      await rpush('events', JSON.stringify(event));
-    }
     eventWorker.batch = 6;
     await eventWorker.start();
     await timeout(250); //hack way to try to let it drain the queue

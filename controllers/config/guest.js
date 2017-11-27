75c336fc242d38423bb4113995d40a5d9e836ceb
Lance Stout <lancestout@gmail.com>
2017-11-27 14:49:34 -0800

    GKE redis does not run over TLS

--- controllers/config/guest.js
+++ controllers/config/guest.js
@@ -58,9 +58,7 @@ module.exports = {
       updated_at: new Date()
     };
 
-    console.log('BEFORE REDIS');
     await redis_rpush('events', JSON.stringify(event));
-    console.log('AFTER REDIS');
 
     const result = {
       id,

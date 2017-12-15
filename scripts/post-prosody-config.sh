aaaf7e6ae7ec322b80d6c99ba3c245bf59227537
Heather Young <heather@andyet.net>
2017-12-15 13:27:52 -0800

    fix delete configmap on restart

--- scripts/post-prosody-config.sh
+++ scripts/post-prosody-config.sh
@@ -2,7 +2,7 @@
 
 # delete existing configmap
 wget --method="DELETE" \
-${KUBERNETES_RO_SERVICE_HOST}:${KUBERNETES_RO_SERVICE_PORT}/api/v1/namespaces/default/configmaps/prosody-config
+${KUBERNETES_RO_SERVICE_HOST}:${KUBERNETES_RO_SERVICE_PORT}/api/v1/namespaces/default/configmaps/prosody-config-${ENV_LEVEL}
 
 # create new configmap
 wget --header="Content-Type: application/yaml" --method="POST" --body-file="/app/cm-prosody-config.json" \

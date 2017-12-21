642219eed30104252950cb5303240fad9ccc3b2b
Lance Stout <lancestout@gmail.com>
2017-12-21 15:42:04 -0800

    Page through events for report generation

--- db/events_for_room.sql
+++ db/events_for_room.sql
@@ -3,5 +3,11 @@ FROM
   events
 WHERE
   events.room_id = ${room_id}
+  AND
+  (${max_id} IS NULL OR events.id < ${max_id})
+  AND
+  (${since_id} IS NULL OR events.id > ${since_id})
 ORDER BY
   events.created_at ASC
+LIMIT ${limit}
+

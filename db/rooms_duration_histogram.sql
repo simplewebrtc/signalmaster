859bec165466880b7a525b7c61a06745491da464
Lance Stout <lancestout@gmail.com>
2018-01-16 15:29:07 -0800

    Add this histogram queries

---
+++ db/rooms_duration_histogram.sql
@@ -0,0 +1,24 @@
+SELECT
+  CASE
+    WHEN ended_at - created_at BETWEEN '0' AND '5s' THEN 'blips'
+    WHEN ended_at - created_at BETWEEN '5s' AND '30s' THEN 'under_30_seconds'
+    WHEN ended_at - created_at BETWEEN '30s' AND '1min' THEN 'under_1_minute'
+    WHEN ended_at - created_at BETWEEN '1min' AND '5min' THEN 'under_5_minutes'
+    WHEN ended_at - created_at BETWEEN '5min' AND '10min' THEN 'under_10_minutes'
+    WHEN ended_at - created_at BETWEEN '10min' AND '20min' THEN 'under_20_minutes'
+    WHEN ended_at - created_at BETWEEN '20min' AND '30min' THEN 'under_30_minutes'
+    WHEN ended_at - created_at BETWEEN '30min' AND '45min' THEN 'under_45_minutes'
+    WHEN ended_at - created_at BETWEEN '45min' AND '1hr' THEN 'under_1_hour'
+    WHEN ended_at - created_at BETWEEN '1hr' AND '2hr' THEN 'under_2_hours'
+    WHEN ended_at - created_at BETWEEN '2hr' AND '4hr' THEN 'under_4_hours'
+    WHEN ended_at - created_at BETWEEN '4hr' AND '8hr' THEN 'under_8_hours'
+    WHEN ended_at - created_at BETWEEN '8hr' AND '12hr' THEN 'under_12_hours'
+    WHEN ended_at - created_at BETWEEN '12hr' AND '24hr' THEN 'under_1_day'
+    ELSE 'over_1_day'
+  END,
+  count(*)
+FROM
+  rooms
+WHERE
+  created_at > ${ts}::timestamp with time zone - 1 * interval ${interval}
+GROUP BY 1
---
+++ db/rooms_occupants_histogram.sql
@@ -0,0 +1,22 @@
+SELECT
+  CASE
+    WHEN reports->'occupants'->>'maxConcurrent' = '1' THEN 'equal_1'
+    WHEN reports->'occupants'->>'maxConcurrent' = '2' THEN 'equal_2'
+    WHEN reports->'occupants'->>'maxConcurrent' = '3' THEN 'equal_3'
+    WHEN reports->'occupants'->>'maxConcurrent' = '4' THEN 'equal_4'
+    WHEN reports->'occupants'->>'maxConcurrent' = '5' THEN 'equal_5'
+    WHEN reports->'occupants'->>'maxConcurrent' = '6' THEN 'equal_6'
+    WHEN reports->'occupants'->>'maxConcurrent' = '7' THEN 'equal_7'
+    WHEN reports->'occupants'->>'maxConcurrent' = '8' THEN 'equal_8'
+    WHEN reports->'occupants'->>'maxConcurrent' = '9' THEN 'equal_9'
+    WHEN reports->'occupants'->>'maxConcurrent' = '10' THEN 'equal_10'
+    ELSE 'over_10'
+  END,
+  count(*)
+FROM
+  rooms
+WHERE
+  reports IS NOT NULL
+  AND
+  created_at > ${ts}::timestamp with time zone - 1 * interval ${interval}
+GROUP BY 1

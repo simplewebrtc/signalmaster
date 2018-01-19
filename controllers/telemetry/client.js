7322f57dc78913c0ef75303221b2a49e7fe994ae
Lance Stout <lancestout@gmail.com>
2018-01-19 15:08:03 -0800

    Accept batched client telemetry

--- controllers/telemetry/client.js
+++ controllers/telemetry/client.js
@@ -9,30 +9,53 @@ module.exports = {
   tags: ['api', 'metrics'],
   handler: async function (request, h) {
 
-    const { eventType, peerId, roomId, data } = request.payload;
+    const { eventType, peerId, roomId, data, batch } = request.payload;
+    if (batch.length === 0) {
+      batch.push({
+        eventType,
+        peerId,
+        roomId,
+        data
+      });
+    }
+
     const session = request.auth.credentials;
     const now = new Date();
-    const event = {
-      created_at: now,
-      updated_at: now,
-      type: eventType,
-      peer_id: peerId,
-      room_id: roomId,
-      actor_id: session.id,
-      data
-    };
-
     const rpush = promisify(this.redis.rpush.bind(this.redis));
-    await rpush('events', JSON.stringify(event));
+
+    const updates = [];
+    for (const item of batch) {
+      const event = {
+        created_at: now,
+        updated_at: now,
+        type: item.eventType,
+        peer_id: item.peerId,
+        room_id: item.roomId,
+        actor_id: session.id,
+        data: item.data
+      };
+
+      updates.push(rpush('events', JSON.stringify(event)));
+    }
+
+    await Promise.all(updates);
 
     return h.response();
   },
   validate: {
     payload: {
-      eventType: Schema.eventTypes,
-      peerId: Joi.string(),
-      roomId: Joi.string(),
-      data: Joi.object().unknown().default({})
+      eventType: Schema.eventTypes.optional(),
+      peerId: Joi.string().optional(),
+      roomId: Joi.string().optional(),
+      data: Joi.object().unknown().default({}).optional(),
+      batch: Joi.array().items(
+        Joi.object({
+          eventType: Schema.eventTypes,
+          peerId: Joi.string(),
+          roomId: Joi.string(),
+          data: Joi.object().unknown().default({})
+        })
+      ).optional().default([])
     }
   },
   auth: 'client-token'

dff89320bdbb01963b705be90339731f8f025ab8
Heather Young <heather@andyet.net>
2018-06-01 15:53:56 -0700

    update prosody gen script to use api instead of sidecar

--- scripts/post-prosody-config.sh
+++ scripts/post-prosody-config.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
 
 # delete existing configmap
-wget --method="DELETE" \
-${KUBERNETES_RO_SERVICE_HOST}:${KUBERNETES_RO_SERVICE_PORT}/api/v1/namespaces/default/configmaps/prosody-config-${ENV_LEVEL}
+
+KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+
+wget -vO- --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt  \
+--header "Authorization: Bearer $KUBE_TOKEN" \
+--method="DELETE" \
+https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/configmaps/prosody-config-${ENV_LEVEL}
 
 # create new configmap
-wget --header="Content-Type: application/yaml" --method="POST" --body-file="/app/cm-prosody-config.json" \
-${KUBERNETES_RO_SERVICE_HOST}:${KUBERNETES_RO_SERVICE_PORT}/api/v1/namespaces/default/configmaps
+wget -vO- --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt  \
+--header="Content-Type: application/yaml" \
+--method="POST" --body-file="/app/cm-prosody-config.json" \
+--header "Authorization: Bearer $KUBE_TOKEN" \
+https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/configmaps

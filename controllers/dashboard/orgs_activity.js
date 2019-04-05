e7635461aca41d6d5a074943627ce6c8b313b6e3
Lance Stout <lancestout@gmail.com>
2019-04-05 10:55:21 -0700

    Clean up per customer activity page.

--- controllers/dashboard/orgs_activity.js
+++ controllers/dashboard/orgs_activity.js
@@ -7,13 +7,67 @@ module.exports = {
   auth: 'admin',
   handler: async function (request, h) {
 
-    const activity = await this.db.orgs_activity();
-    const day = await this.db.activity_summary({ interval: '1 days' });
-    const week = await this.db.activity_summary({ interval: '7 days' });
+
+    const createOrgRecord = () => ({
+      config_request_count: 0,
+      used_turn_count: 0,
+      session_connected_count: 0,
+      web_count: 0,
+      mobile_count: 0
+    });
+
+    const totalStats = {
+      active: createOrgRecord(),
+      one_day: createOrgRecord(),
+      week: createOrgRecord()
+    };
+    const perCustomerStats = new Map();
+
+    const categorize = (org, data, field) => {
+
+      totalStats[field].config_request_count += data.config_request_count;
+      totalStats[field].used_turn_count += data.used_turn_count;
+      totalStats[field].session_connected_count += data.session_connected_count;
+      totalStats[field].web_count += data.web_count;
+      totalStats[field].mobile_count += data.mobile_count;
+
+      let orgStats = perCustomerStats.get(org);
+      if (!orgStats) {
+        orgStats = {
+          id: org,
+          active: createOrgRecord(),
+          one_day: createOrgRecord(),
+          week: createOrgRecord(),
+          last_activity: undefined
+        };
+        perCustomerStats.set(org, orgStats);
+      }
+
+      orgStats[field].config_request_count += data.config_request_count;
+      orgStats[field].used_turn_count += data.used_turn_count;
+      orgStats[field].session_connected_count += data.session_connected_count;
+      orgStats[field].web_count += data.web_count;
+      orgStats[field].mobile_count += data.mobile_count;
+
+      if (data.last_activity && data.last_activity > orgStats.last_activity) {
+        orgStats.last_activity = data.last_activity;
+      }
+    };
+
+    const activity = await this.db.sessions.per_customer_stats();
+    for (const data of activity) {
+      if (data.live) {
+        categorize(data.org_id, data, 'active');
+      }
+      if (data.one_day) {
+        categorize(data.org_id, data, 'one_day');
+      }
+      categorize(data.org_id, data, 'week');
+    }
+
     return h.view('orgs_activity', {
-      day: day[0],
-      week: week[0],
-      activity
+      totalStats,
+      perCustomerStats: [...perCustomerStats.values()]
     });
   },
   validate: {
--- db/sessions_per_customer_stats.sql
+++ db/sessions_per_customer_stats.sql
@@ -3,8 +3,8 @@ SELECT
     count(*)::integer as config_request_count,
     count(used_turn OR NULL)::integer as used_turn_count,
     count(activated OR NULL)::integer as session_connected_count,
-    sum(CASE WHEN type = 'desktop' and activated = true THEN 1 ELSE 0 END) as web_count,
-    sum(CASE WHEN type = 'mobile' and activated = true THEN 1 ELSE 0 END) as mobile_count,
+    sum(CASE WHEN type = 'desktop' and activated = true THEN 1 ELSE 0 END)::integer as web_count,
+    sum(CASE WHEN type = 'mobile' and activated = true THEN 1 ELSE 0 END)::integer as mobile_count,
     max(created_at) as last_activity,
     CASE
       WHEN ended_at IS NULL THEN true
